e1dfaf29c0e508860a58675c4305e59d95fe8087
Hem Raj Joshi <hemrajjoshi2076@gmail.com>
2026-07-26 14:41:44 +0545

    Business page added

--- src/app/contact-for-business/page.tsx
+++ src/app/contact-for-business/page.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { FaUser } from "react-icons/fa";
+import { MdUpdate } from "react-icons/md";
+import { useSelector } from "react-redux";
 
-type Props = {}
+const BusinessPage = () => {
+  const { user } = useSelector((state: any) => state.auth);
+  const router = useRouter();
 
-const Business = (props: Props) => {
+  useEffect(() => {
+    if (!user) {
+      router.push("/");
+    }
+  }, [user]);
   return (
-    <div>Business Page</div>
-  )
-}
+    <>
+      <section className="h-full flex items-center justify-center text-center">
+        <div className="container">
+          <div className="flex justify-center items-center size-30 mx-auto bg-white shadow rounded-full text-7xl text-gray-600 mb-7">
+            <MdUpdate />
+          </div>
+          {user && (
+            <h2>
+              Dear, <span className="text-primary">{user.name}</span>
+            </h2>
+          )}
+          <h3 className="h4">New updates are on the way 🚀</h3>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Business
+export default BusinessPage;
